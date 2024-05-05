bef4c613ec2ee2e3b15183a1707c6c7c365e4e72
KhomyakovDaniil <daniil.h@appomart.com>
2024-05-05 10:39:03 +0300

    Initial Commit

---
+++ SpeedTestMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpeedTestMVVM
+//
+//  Created by  Даниил Хомяков on 05.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
